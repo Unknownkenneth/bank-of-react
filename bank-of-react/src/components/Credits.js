321c37810859f8dbfd7449f757ba7e2918f986a8
Kenneth Louis <lowkam4@gmail.com>
2022-11-27 15:59:39 -0500

    Update Credits.js

--- bank-of-react/src/components/Credits.js
+++ bank-of-react/src/components/Credits.js
@@ -11,26 +11,26 @@ import {Link} from 'react-router-dom';
 
 
 class Credits extends Component {
-    render() {
-        let credits = this.props.credits;
-        return (
-            <div className='background'>
-                <h1 className='title'>Credits</h1>
-                <div className='history'>
-                    {credits.map(credit => (
-                        <div key={credit.id}>
-                          <li>{credit.amount} {credit.description} {credit.date.slice(0, 10)}</li>
-                        </div> ))}
-                <div className='balance'><AccountBalance accountBalance={this.props.accountBalance}/></div>
-                <form onSubmit={this.props.addCredit}>
-                 <p>
-                      <label id="description"> Description: </label>
-                     <input type="text" name="description"/>
-                     <label id="amount"> Amount: </label>
-                     <input type="number" name="amount"/>
+   render() {
+    let credits = this.props.credits;
+      return (
+         <div className='background'>
+            <h1 className='title'>Credits</h1>
+            <div className='history'>
+                {credits.map(credit => (
+                  <div key={credit.id}>
+                       <li>{credit.amount} {credit.description} {credit.date.slice(0, 10)}</li>
+                  </div> ))}
+                  <div className='balance'><AccountBalance accountBalance={this.props.accountBalance}/></div>
+                  <form onSubmit={this.props.addCredit}>
+                    <p>
+                       <label id="description"> Description: </label>
+                        <input type="text" name="description"/>
+                        <label id="amount"> Amount: </label>
+                        <input type="number" name="amount"/>
                        <button type="submit"> Submit</button>
-                  </p>
-                </form>
+                    </p>
+                  </form>
 <div className='link'>
   <Link to="/"> Home </Link>
   <Link to="/Login"> Login </Link>
